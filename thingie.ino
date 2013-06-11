fc6b21a7d8ef8decd6fdd5df17537b0e8b7c25e0
Michael D'Auria <michael.dauria@gmail.com>
2013-06-10 22:33:46 -0400

    Second pass, fully working with proper states and debouncing

--- thingie.ino
+++ thingie.ino
@@ -1,40 +1,124 @@
-int blue     = 9;  // BLUE
-int green    = 10; // GREEN
-int red      = 11; // RED
-int silence  = 12; // SILENCE button
-int presence = 13; // PRESENCE button
+/**
+ * Crowdtap Thingie
+ * Hilal Koyuncu & Michael D'Auria
+ */
 
-boolean present = false;
-boolean quiet   = false;
+const boolean commonAnode = false;
+
+const byte btnSilence  = 2; // INT0 & SILENCE button
+const byte btnPresence = 3; // INT1 & PRESENCE button
+
+const byte ledBlue  =  9; // BLUE
+const byte ledGreen = 10; // GREEN
+const byte ledRed   = 11; // RED
+
+static byte present = false;
+static byte quiet   = false;
+
+static byte prevPresent = false;
+static byte prevQuiet   = false;
+
+const unsigned int quietTime = 10*1000; // s
+static unsigned long quietTimer = 0;
+
+const byte debounce = 1200; // ms
 
 void setup()  {
-  pinMode(red, OUTPUT);
-  pinMode(green, OUTPUT);
-  pinMode(blue, OUTPUT);
+  pinMode(ledRed,   OUTPUT);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledBlue,  OUTPUT);
 
-  pinMode(silence, INPUT);
-  pinMode(presence, INPUT);
+  pinMode(btnPresence, INPUT);
+  digitalWrite(btnPresence, HIGH);
+  attachInterrupt(0, togglePresence, FALLING);
+
+  pinMode(btnSilence, INPUT);
+  digitalWrite(btnSilence, HIGH);
+  attachInterrupt(1, toggleQuiet, FALLING);
+
+  setAway();
+  Serial.begin(9600);
 }
 
 void loop() {
-  if (present) {
-    if(quiet) {
-      setColor(0,0,60);
+  if (prevPresent != present || prevQuiet != quiet) {
+    if (present) {
+      if (quiet) {
+        setQuiet();
+      } else {
+        setPresent();
+      }
     } else {
-      setColor(0,60,0);
+      setAway();
     }
-  } else {
-    setColor(255,0,0);
+
+    prevPresent = present;
+    prevQuiet = quiet;
   }
 
-  quiet   = digitalRead(silence);
-  present = digitalRead(presence);
+  if (quietTimer > 0 && millis() > quietTimer) {
+    toggleQuiet();
+  }
 }
 
-void setColor(int r, int g, int b) {
-  analogWrite(red, r);
-  analogWrite(green, g);
-  analogWrite(blue, b);  
+void setColor(byte red, byte green, byte blue) {
+  if (commonAnode) {
+    red   = 255-red;
+    green = 255-green;
+    blue  = 255-blue;
+  }
+
+  red   = constrain(red,   0, 255);
+  green = constrain(green, 0, 255);
+  blue  = constrain(blue,  0, 255);
+
+  analogWrite(ledRed,   red);
+  analogWrite(ledGreen, green);
+  analogWrite(ledBlue,  blue);
 }
 
+void setAway() {
+  Serial.println("away");
+  setColor(25, 0, 0);
+  present = false;
+}
 
+void setPresent() {
+  Serial.println("present");
+  setColor(0, 25, 0);
+  present = true;
+  quiet = false;
+}
+
+void setQuiet() {
+  Serial.println("quiet");
+  setColor(0, 0, 25);
+  quiet = true;
+}
+
+void togglePresence() {
+  static unsigned long lastInterruptTime0 = 0;
+  unsigned long interruptTime0 = millis();
+
+  if (interruptTime0 - lastInterruptTime0 > debounce) {
+    if (quiet) {
+      toggleQuiet();
+    }
+
+    present = !present;
+  }
+
+  lastInterruptTime0 = interruptTime0;
+}
+
+void toggleQuiet() {
+  static unsigned long lastInterruptTime1 = 0;
+  unsigned long interruptTime1 = millis();
+
+  if (interruptTime1 - lastInterruptTime1 > debounce) {
+    quiet = !quiet;
+    quietTimer = (quiet) ? (interruptTime1 + quietTime) : 0;
+  }
+
+  lastInterruptTime1 = interruptTime1;
+}
